f442ed163b88388eee70abe6315f90905f38b098
Yeison Vargas <yeisonvargasjf@gmail.com>
2022-06-14 14:47:30 -0500

    Using click

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import click
 
-click.secho('Hello World without whitespace', fg='green')
-click.secho('  Hello World with styled whitespace', fg='green');
-click.echo('  ' + click.style('Hello World with non-styled whitespace', fg='green'))
+@click.command()
+def hello():
+    click.secho('Hello World without whitespace', fg='green')
+    click.secho('  Hello World with styled whitespace', fg='green');
+    click.echo('  ' + click.style('Hello World with non-styled whitespace', fg='green'))
+
+
+if __name__ == '__main__':
+    hello()
+
